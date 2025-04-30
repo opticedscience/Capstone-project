2ec822f2ef1e586b75d5c0b9a10e5330f259d933
opticed <optics2datascience@gmail.com>
2025-04-29 20:31:28 -0700

    added static content

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def sayHello(request):
+   return HttpResponse('Hello World')
+
+def index(request):
+  return render(request, 'index.html', {})
+
+def home(request):
+  return render(request, 'home.html',{})
